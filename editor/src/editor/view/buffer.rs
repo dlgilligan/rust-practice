f1d4bc22c837c3b0324b259222f8541ec923eb28
dlgilligan <danny.gilligan26@gmail.com>
2025-03-21 18:35:23 -0400

    Scrolling functionality. 10.

--- editor/src/editor/view/buffer.rs
+++ editor/src/editor/view/buffer.rs
@@ -1,9 +1,11 @@
 use std::fs::read_to_string;
 use std::io::Error;
 
+use super::line::Line;
+
 #[derive(Default)]
 pub struct Buffer {
-    pub lines: Vec<String>,
+    pub lines: Vec<Line>,
 }
 
 impl Buffer {
@@ -12,7 +14,7 @@ impl Buffer {
         let mut lines = Vec::new();
 
         for value in contents.lines() {
-            lines.push(String::from(value));
+            lines.push(Line::from(value));
         }
         Ok(Self { lines })
     }
