d37b41fcddb2407a096943ab15f7b452dd04ae94
dlgilligan <danny.gilligan26@gmail.com>
2025-03-22 23:25:48 -0400

    DynamoDB stuff

--- ActixWebTaskService/src/api/task.rs
+++ ActixWebTaskService/src/api/task.rs
@@ -9,13 +9,23 @@ use actix_web::{
     HttpResponse,
 };
 use derive_more::Display;
-use serd::{Deserialize, Serialize};
+use serde::{Deserialize, Serialize};
+
+// Field name has to match that of the path parameter
+#[derive(Serialize, Deserialize)]
+pub struct TaskIdentifier {
+    task_global_id: String,
+}
 
 // Handler function. Tied to path and HTTP method.
 // Handler function has to return one of two things. A struct that implements the Responder trait,
 // or a result for which the success value implements the Responder trait and the error value
-// implements ResponseError
+// implements ResponseError.
+// Extractors are a means of getting at various arts of the request from within the handler
+// function. We use extractors by adding parameters to the handler function and if those parameters
+// implement the FromRequest trait, thats considered an extractor and actix web framework will
+// automatically populate those parameters with the appropriate values.
 #[get("/task/{task_global_id}")]
-pub async fn get_task() -> Json<String> {
-    return Json("hello world".to_string());
+pub async fn get_task(task_identifier: Path<TaskIdentifier>) -> Json<String> {
+    return Json(task_identifier.into_inner().task_global_id);
 }
