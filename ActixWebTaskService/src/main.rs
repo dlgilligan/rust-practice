7afd40088e24a822982f08de357ea2ed3c626369
dlgilligan <danny.gilligan26@gmail.com>
2025-03-23 00:44:00 -0400

    Injecting DDB client into handler functions

--- ActixWebTaskService/src/main.rs
+++ ActixWebTaskService/src/main.rs
@@ -1,7 +1,9 @@
 mod api;
+mod repository;
 use api::task::get_task;
 
 use actix_web::{middleware::Logger, web::Data, App, HttpServer};
+use repository::ddb::DDBRepository;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -10,11 +12,21 @@ async fn main() -> std::io::Result<()> {
     std::env::set_var("RUST_BACKTRACE", "1");
     env_logger::init();
 
+    let config = aws_config::load_from_env().await;
     // Pass in closure that sets up everything for the web application
     // Closure is ran everytime actix starts a new thread
     HttpServer::new(move || {
+        let ddb_repo::DDBRepository::init(
+            String::from("task"),
+            config.clone(), // Create a copy for every thread
+        );
+        let ddb_data = Data::new(ddb_repo); // To pass shared data, need to use data struct that
+        // implements FromRequest trait
         let logger = Logger::default();
-        App::new().wrap(logger).service(get_task)
+        App::new()
+            .wrap(logger)
+            .app_data(ddb_data) // Shared state that will be injected into handler functions
+            .service(get_task)
     })
     .bind(("127.0.0.1", 80))?
     .run()
